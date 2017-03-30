7cc13cd54460ad337042dd54e208860c87badaa8
Guðni Natan Gunnarsson <gudni.nathan@gmail.com>
2017-03-30 16:51:54 +0000

    More comments

--- demo.py
+++ demo.py
@@ -18,12 +18,12 @@ pygame.display.set_caption('Better timer demo')
 timers = BetterTimers() #You need to define the BetterTimers object
 
 
-timers.set_timer(pygame.event.Event(genericEvent, code="cool"), 500)
-timers.set_timer(pygame.event.Event(genericEvent, code="uncool"), 1000)
-timers.set_timer(pygame.event.Event(genericEvent, code="cool"), 2000)
+timers.set_timer(pygame.event.Event(genericEvent, code="cool"), 500) # Sets a timer for 500 milliseconds
+timers.set_timer(pygame.event.Event(genericEvent, code="uncool"), 1000) # New genericEvent timer with a different code
+timers.set_timer(pygame.event.Event(genericEvent, code="cool"), 2000) # Overrides old 500 rate timer
 
-timers.set_timer(randomEvent, 5000)
-timers.set_timer(randomEvent, 0)
+timers.set_timer(randomEvent, 5000) # Traditional timer set where there are no extra arguments
+timers.set_timer(randomEvent, 0) #Timer stopped
 
 
 running = True
