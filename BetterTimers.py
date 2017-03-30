3664d3fd5740a115cb012c1014e2cddd4f6e3265
Guðni Natan Gunnarsson <gudni.nathan@gmail.com>
2017-03-30 15:41:35 +0000

    Initial

---
+++ BetterTimers.py
@@ -0,0 +1,65 @@
+import threading
+import pygame
+
+# By Gudni Natan Gunnarsson, 2017
+
+class Timer(object):
+    def __init__(self, event, rate):
+        self.running = False
+        self.event = event
+        self.rate = rate
+        self.t = threading.Thread(target=self.eventPoster, args=(event, rate))
+        self.t.daemon = True
+        self.ok = threading.Event()
+
+    def go(self):
+        self.ok.set()
+
+    def eventPoster(self, event, rate):
+        e = pygame.event.Event
+        while self.running:
+            if type(event) is pygame.event.EventType:
+                pygame.event.post(event)
+            else:
+                pygame.event.post(pygame.event.Event(event))
+            self.ok.clear()
+            t2 = threading.Timer(float(rate) / 1000.0, self.go)
+            t2.start()
+            self.ok.wait()
+
+    def start_timer(self):
+        self.running = True
+        self.t.start()
+
+    def stop_timer(self):
+        if self.running:
+            self.running = False
+            self.ok.set()
+            self.t.join()
+
+    def change_rate(self, rate):
+        self.rate = rate
+
+class BetterTimers(object):
+    def __init__(self):
+        self.timers = list()
+
+    def set_timer(self, event, rate):
+        t = Timer(event, rate)
+        for e in self.timers:
+            if e.event == event:
+                if rate > 0:
+                    e.change_rate(rate)
+                else:
+                    e.stop_timer()
+                    self.timers.remove(e)
+                return
+        if rate > 0:
+            t.start_timer()
+            self.timers.append(t)
+
+    def end_all_timers(self):
+        for t in self.timers:
+            t.stop_timer()
+
+        self.timers = list()
---
+++ timers.py
@@ -0,0 +1,47 @@
+import pygame
+from pygame.locals import *
+from BetterTimers import BetterTimers
+import sys
+import os
+import threading
+
+
+
+
+genericEvent = USEREVENT + 1
+randomEvent = USEREVENT + 2
+
+window_size = window_width, window_height = 1280, 720
+pygame.init()
+screen = pygame.display.set_mode(window_size, pygame.DOUBLEBUF)
+clock = pygame.time.Clock()
+pygame.display.set_caption('Better timer demo')
+
+timers = BetterTimers()
+
+
+timers.set_timer(pygame.event.Event(genericEvent, code="cool"), 500)
+timers.set_timer(pygame.event.Event(genericEvent, code="uncool"), 1000)
+timers.set_timer(pygame.event.Event(genericEvent, code="cool"), 2000)
+
+timers.set_timer(randomEvent, 5000)
+timers.set_timer(randomEvent, 0)
+
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == K_ESCAPE):
+            running = False
+            timers.end_all_timers() #We stop all the timers
+            pygame.quit()
+            break
+        if event.type == randomEvent:
+            print("WOWZERS")
+        if event.type == genericEvent and event.code == 'cool':
+            print("COOL EVENT")
+        if event.type == genericEvent and event.code == 'uncool':
+            print("SO UNCOOL")
+    clock.tick(1000)  # It is good game design to try to limit the fps as little as possible.
+pygame.quit()
